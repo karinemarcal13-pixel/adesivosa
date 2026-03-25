992fabe7e99bb1568c23f3883ad0fb00f0962715
karinemarcal13-pixel <karinemarcal13@gmail.com>
2026-03-24 22:00:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
+function enviarWhats(event) {
+  event.preventDefault();
 
-  const elementos = document.querySelectorAll("section");
+  let nome = document.getElementById("nome").value;
+  let servico = document.getElementById("servico").value;
 
-  function animar() {
-    let topoTela = window.innerHeight;
-
-    elementos.forEach(el => {
-      let posicao = el.getBoundingClientRect().top;
-
-      if (posicao < topoTela - 100) {
-        el.classList.add("mostrar");
-      }
-    });
-  }
-
-  window.addEventListener("scroll", animar);
-  animar();
-
-  const campoWhatsapp = document.querySelector("input[name='whatsapp']");
-
-  campoWhatsapp.addEventListener("input", function(e) {
-    let v = e.target.value.replace(/\D/g, "");
-
-    v = v.replace(/^(\d{2})(\d)/g, "($1) $2");
-    v = v.replace(/(\d{5})(\d)/, "$1-$2");
-
-    e.target.value = v;
-  });
-
-});
-
-function enviarWhatsApp(nome, tipo, mensagem) {
-  let texto = `Olá! Me chamo ${nome}. Quero um orçamento para ${tipo}. ${mensagem}`;
-  let url = `https://wa.me/5543998721117?text=${encodeURIComponent(texto)}`;
-  window.open(url, "_blank");
-}
-
-function validarFormulario(e) {
-  e.preventDefault();
-
-  let nome = document.querySelector("input[name='nome']").value.trim();
-  let whatsapp = document.querySelector("input[name='whatsapp']").value.trim();
-  let tipo = document.querySelector("select[name='tipo']").value;
-  let mensagem = document.querySelector("textarea[name='mensagem']").value;
-
-  if (!nome || !whatsapp) {
-    alert("Preencha nome e WhatsApp!");
+  if (nome === "" || servico === "") {
+    alert("Preencha tudo 😅");
     return;
   }
 
-  enviarWhatsApp(nome, tipo, mensagem);
+  let numero = "5543984046323";
+
+  let mensagem = `Olá, tudo bem?
+Meu nome é ${nome}
+Quero um orçamento
+
+📍 Atendimento: Londrina e região
+
+Serviço: ${servico}`;
+
+  let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+  window.open(url, "_blank");
 }
